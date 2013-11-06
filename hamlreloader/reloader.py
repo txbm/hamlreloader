6dc30c2daff809cb162f5a629a1c14330d1022a5
Peter M. Elias <petermelias@gmail.com>
2013-11-06 14:04:37 -0600

    fixed up some stuff

--- hamlreloader/reloader.py
+++ hamlreloader/reloader.py
@@ -1,10 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
 import logging
 
 from time import sleep
 
 from os import makedirs
-from os.path import (splitext, abspath, dirname, 
-	basename, join, relpath, exists)
+from os.path import (
+    splitext,
+    abspath,
+    dirname,
+    basename,
+    join,
+    relpath,
+    exists
+)
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
@@ -13,60 +23,65 @@ from subprocess import call
 
 logger = logging.getLogger('hamlreloader')
 
+
 def parse_file_ext(p):
-	return splitext(p)[-1].lower()
+    return splitext(p)[-1].lower()
+
 
 def watch_directory(watch_dir, target_dir, condition=None):
-	if condition:
-		condition.acquire()
-	
-	watch_path = abspath(watch_dir)
-	logger.info('Watch path: %s' % watch_path)
-	
-	target_path = abspath(target_dir)
-	logger.info('Target path: %s' % target_path)
+    if condition:
+        condition.acquire()
 
-	handler = ModifiedHandler(watch_path, target_path)
-	obs = Observer()
+    watch_path = abspath(watch_dir)
+    logger.info('Watch path: %s' % watch_path)
 
-	obs.schedule(handler, watch_path, recursive=True)
-	obs.start()
-	
-	if condition:
-		condition.notify()
-		condition.release()
-	try:
-		while True:
-			sleep(1)
-	except KeyboardInterrupt:
-		obs.stop()
-	obs.join()
+    target_path = abspath(target_dir)
+    logger.info('Target path: %s' % target_path)
+
+    handler = ModifiedHandler(watch_path, target_path)
+    obs = Observer()
+
+    obs.schedule(handler, watch_path, recursive=True)
+    obs.start()
+
+    if condition:
+        condition.notify()
+        condition.release()
+    try:
+        while True:
+            sleep(1)
+    except KeyboardInterrupt:
+        obs.stop()
+    obs.join()
+
 
 def render_haml(haml_file, dest_file):
-	cmd = r'haml %s %s' % (haml_file, dest_file)
-	call(cmd, shell=True)
+    cmd = r'haml %s %s' % (haml_file, dest_file)
+    call(cmd, shell=True)
+
 
 class ModifiedHandler(FileSystemEventHandler):
 
-	def __init__(self, watch_path, target_path):
-		self.watch_path = watch_path
-		self.target_path = target_path
-		super(ModifiedHandler, self).__init__()
+    def __init__(self, watch_path, target_path):
+        self.watch_path = watch_path
+        self.target_path = target_path
+        super(ModifiedHandler, self).__init__()
 
-	def on_any_event(self, e):
-		ext = parse_file_ext(e.src_path)
-		if not ext == '.haml':
-			return
-		
-		logger.info('Detected change in: %s' % e.src_path)
+    def on_any_event(self, e):
+        ext = parse_file_ext(e.src_path)
+        if not ext == '.haml':
+            return
 
-		subpath = dirname(relpath(e.src_path, self.watch_path))
-		write_dir = join(self.target_path, subpath)
-		
-		if not exists(write_dir):
-			makedirs(write_dir)
-		
-		write_path = join(write_dir, '%s.html' % splitext(basename(e.src_path))[0])
+        logger.info('Detected change in: %s' % e.src_path)
 
-		logger.info('Writing to: %s' % write_path)
-		render_haml(e.src_path, write_path)
+        subpath = dirname(relpath(e.src_path, self.watch_path))
+        write_dir = join(self.target_path, subpath)
+
+        if not exists(write_dir):
+            makedirs(write_dir)
+
+        write_path = join(write_dir, '%s.html' %
+                          splitext(basename(e.src_path))[0])
+
+        logger.info('Writing to: %s' % write_path)
+        render_haml(e.src_path, write_path)
--- hamlreloader/tests/test_reloader.py
+++ hamlreloader/tests/test_reloader.py
@@ -1,54 +1,96 @@
-import logging
-from time import sleep
-from os import unlink, listdir
-from os.path import realpath, dirname, join, exists
+# -*- coding: utf-8 -*-
 
-from nose.tools.trivial import assert_equal, assert_true
-from multiprocessing import Process, Lock, Condition
+
+import logging
+
+from time import sleep
+
+from shutil import rmtree
+
+from os import (
+    unlink,
+    listdir,
+    mkdir
+)
+
+from os.path import (
+    realpath,
+    dirname,
+    join,
+    exists,
+    isdir
+)
+
+from nose.tools.trivial import (
+    assert_equal,
+    assert_true
+)
+
+from multiprocessing import (
+    Process,
+    Condition
+)
 
 from hamlreloader import reloader
 
 logging.basicConfig(level=logging.INFO)
 
+
 def test_parse_file_ext():
-	filename = 'lol.jpg'
-	assert_equal(reloader.parse_file_ext(filename), '.jpg')
+    filename = 'lol.jpg'
+    assert_equal(reloader.parse_file_ext(filename), '.jpg')
+
 
 def test_watch_directory():
-	sample_template = ''
-	sample_directory = dirname(realpath(__file__)) + '/sample/'
-	watch_directory = sample_directory + 'watch/'
-	render_directory = sample_directory + 'render/'
-	template_directory = sample_directory + 'templates/'
-	with open(template_directory + 'haml.tmpl', 'r') as f:
-		sample_template = f.read()
-	
-	condition = Condition()
-	p = Process(target=reloader.watch_directory, args=(watch_directory, render_directory, condition))
-	condition.acquire()
-	p.start()
-	condition.wait()
-	
-	try:
-		with open(watch_directory + 'test.haml', 'w') as f:
-			f.write(sample_template)
 
-		sleep(1)
+    def _cleanup(path):
+        for f in listdir(path):
+            p = join(path, f)
+            if isdir(p):
+                rmtree(p)
+            elif f != '.nothing':
+                unlink(p)
 
-		assert_true(exists(render_directory + 'test.html'))
-	except:
-		raise
-	finally:
-		condition.release()
-		p.terminate()
-		p.join()
+    sample_template = ''
+    sample_directory = dirname(realpath(__file__)) + '/sample/'
+    watch_directory = sample_directory + 'watch/'
+    render_directory = sample_directory + 'render/'
+    template_directory = sample_directory + 'templates/'
+    with open(template_directory + 'haml.tmpl', 'r') as f:
+        sample_template = f.read()
 
-		sleep(1)
+    condition = Condition()
+    p = Process(target=reloader.watch_directory,
+                args=(watch_directory, render_directory, condition))
+    condition.acquire()
+    p.start()
+    condition.wait()
 
-		for f in listdir(watch_directory):
-			if f != '.nothing':
-				unlink(join(watch_directory, f))
+    try:
+        with open(watch_directory + 'test.haml', 'w') as f:
+            f.write(sample_template)
 
-		for f in listdir(render_directory):
-			if f != '.nothing':
-				unlink(join(render_directory, f))
+        subdir = watch_directory + 'test_subdir/'
+        try:
+            mkdir(subdir)
+        except OSError:
+            if not isdir(subdir):
+                raise
+        with open(subdir + 'test_two.haml', 'w') as f:
+            f.write(sample_template)
+
+        sleep(1)
+
+        assert_true(exists(render_directory + 'test.html'))
+        assert_true(exists(render_directory + 'test_subdir/test_two.html'))
+    except:
+        raise
+    finally:
+        condition.release()
+        p.terminate()
+        p.join()
+
+        sleep(1)
+
+        _cleanup(watch_directory)
+        _cleanup(render_directory)
